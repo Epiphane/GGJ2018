b7b3fcb010f7d2ec42d0e5da0a4474e654a6e406
Epiphane <exyphnos@gmail.com>
2018-01-26 20:12:47 -0800

    5 pings and then youre donezo

---
+++ Assets/Scripts/AudioSourceScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceScript : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public GameObject pingPrefab;
+
+    public float repeatDelay = 2.0f;
+    public bool playOnAwake = true;
+
+    public int numPings = 5;
+    public float delayAddPerPing = 0.5f;
+
+    private float cooldown = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        if (audioSource == null) {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        audioSource.playOnAwake = false;
+
+        if (!playOnAwake) {
+            cooldown = repeatDelay;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cooldown -= Time.deltaTime;
+        if (cooldown <= 0) {
+            Ping();
+        }
+    }
+
+    public void Ping() {
+        if (numPings <= 0) {
+            return;
+        }
+
+        audioSource.Play();
+
+        SpawnLight();
+        repeatDelay += delayAddPerPing;
+        cooldown = repeatDelay;
+        numPings--;
+    }
+
+    void SpawnLight () {
+        GameObject ping = GameObject.Instantiate(pingPrefab);
+
+        ping.transform.position = transform.position;
+        ping.transform.parent = transform;
+    }
+}
---
+++ Assets/Scripts/FadeAndExpandScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeAndExpandScript : MonoBehaviour {
+
+    // Lifetime in seconds
+    public float lifetime = 1.0f;
+    new public SpriteRenderer renderer;
+
+    private Vector3 finalScale = Vector3.zero;
+    private float lifeRemaining = 1.0f;
+
+    // Use this for initialization
+    void Start() {
+        lifeRemaining = lifetime;
+
+        finalScale = transform.localScale;
+        transform.localScale = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        lifeRemaining -= Time.deltaTime;
+
+        float opacity = lifeRemaining / lifetime;
+        if (lifeRemaining < 0) {
+            GameObject.Destroy(gameObject);
+        }
+
+        transform.localScale = (1 - opacity) * finalScale;
+        renderer.color = new Color(1, 1, 1, opacity);
+    }
+}
