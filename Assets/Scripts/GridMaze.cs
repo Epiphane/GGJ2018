0f18612e09b1e5a1efaddebd114daef82e01d6d6
Ernesto Rojo <gamedev@ernestorojo.com>
2018-01-27 13:51:55 -0800

    Adds a GridMaze script to the Grid which allows us to turn the maze sprites on and off

---
+++ Assets/Scripts/GridMaze.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMaze : MonoBehaviour 
+{
+    [SerializeField]
+    private bool _areGridTilesVisible = true;
+
+	// Use this for initialization
+	void Start () 
+    {
+		var sprites = GetComponentsInChildren(typeof(SpriteRenderer));
+
+		
+		if (sprites != null)
+        {
+			foreach(var gridTile in sprites)
+            {
+                if (_areGridTilesVisible)
+                    gridTile.gameObject.SetActive(true);
+                else
+                    gridTile.gameObject.SetActive(false);
+            }
+        }
+	}
+}
