ab252eb24f1f3426ad3f4e6b8691912598d713dc
Ernesto Rojo Jr <gamedev@ernestorojo.com>
2018-01-28 13:03:09 -0800

    Updated Monster spawner to spawn monster at it's location

--- Assets/Scripts/MonsterSpawner.cs
+++ Assets/Scripts/MonsterSpawner.cs
@@ -7,24 +7,14 @@ public class MonsterSpawner : MonoBehaviour
     [SerializeField]
     private GameObject _monsterPrefab;
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == Tags.Player && _monsterPrefab)
         {
             var monsterGO = Instantiate(_monsterPrefab);
             var monsterAI = monsterGO.GetComponent<MonsterAI>();
+
+            monsterGO.transform.SetParent(transform, false);
             monsterAI.StartFollowingPlayer();
         }
     }
